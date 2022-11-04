a32f8f47a70503b8bf625aca945bbab10718ee5d
yogiraam <114808186+yogiraam@users.noreply.github.com>
2022-11-04 09:24:41 +0530

    Add files via upload

---
+++ SpringBootGradleApp5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abc.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGradleApp5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
